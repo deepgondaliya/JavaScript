519fd4e2003e58dbf511983732700743bc312f47
deepgondaliya <133983843+deepgondaliya@users.noreply.github.com>
2023-12-27 18:17:42 +0000

    Arrow function

---
+++ 03_basics/arrowfunction.js
@@ -0,0 +1,52 @@
+//use of this operator
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// not use this keyword in function or arrow function
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+//use simple bracket not write to return statement
+// const addTwo = (num1, num2) => ( num1 + num2 ) 
+
+//object create in arrow function
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+console.log(addTwo(3, 4))
