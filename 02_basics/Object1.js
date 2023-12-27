fece216f39ddb4758ca0c23664c301e23faa35f2
deepgondaliya <133983843+deepgondaliya@users.noreply.github.com>
2023-12-27 16:16:45 +0000

    Objects

---
+++ 02_basics/Object1.js
@@ -0,0 +1,38 @@
+//Non singleton
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Deep Gondaliya",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"]) // not use (.) dot-operator
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+// create function
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
